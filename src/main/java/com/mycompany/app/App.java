8b790e2494043e5dc6961488bf48c1871526937f
Vince Tylke <35105275+Tylke@users.noreply.github.com>
2021-04-22 14:00:17 -0500

    commit for version 3.0
    commit for version 3.0

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!v2.0" );
+        System.out.println( "Hello World!!1!v3.0" );
     }
 }
